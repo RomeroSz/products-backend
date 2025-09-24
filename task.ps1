1aef9d6c19f8abf179eca80f97e77362458b1a9a
Victor Romero <victor.romero@sudeaseg.gob.ve>
2025-09-24 08:09:11 -0400

    Controles y configuracion inicial

---
+++ task.ps1
@@ -0,0 +1,9 @@
+﻿param([string]\)
+switch (\) {
+  "run"      { poetry run python manage.py runserver 0.0.0.0:8000 }
+  "migrate"  { poetry run python manage.py migrate }
+  "super"    { poetry run python manage.py createsuperuser }
+  "lint"     { poetry run ruff check . --fix; poetry run isort .; poetry run black . }
+  "collect"  { poetry run python manage.py collectstatic --noinput }
+  default    { Write-Host "Usage: .\task.ps1 [run|migrate|super|lint|collect]" }
+}
